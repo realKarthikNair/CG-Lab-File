e7a525aa368b8d41d6a6e35459cc260d2fa11a94
Karthik Nair <realkarthiknair@gmail.com>
2023-11-28 02:12:21 +0530

    core9

---
+++ lab file/programs/core9.cpp
@@ -0,0 +1,34 @@
+#include <graphics.h>
+
+void drawOriginalShape() {
+    setcolor(RED);
+    rectangle(100, 100, 200, 200);
+}
+
+void drawShearedShape() {
+    setcolor(BLUE);
+
+    int shearX = 1; 
+    int shearY = 0; 
+    int x1 = 100 + shearX * 100;
+    int y1 = 100 + shearY * 100;
+    int x2 = 200 + shearX * 100;
+    int y2 = 200 + shearY * 100;
+
+    rectangle(x1, y1, x2, y2);
+}
+
+int main() {
+    int gd = DETECT, gm;
+    initgraph(&gd, &gm, NULL);
+
+    drawOriginalShape();
+    delay(2000);
+    cleardevice();
+    
+    drawShearedShape();
+
+    getch();
+    closegraph();
+    return 0;
+}
